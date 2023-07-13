1bcbe1f055bbcba06fd40c357c76313293f67b67
Erynn Affeldt <erynn.affeldt@gmail.com>
2023-07-13 16:32:18 -0400

    Got application up and running

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/NavBar';
+import Navbar from '../components/Navbar';
 import { Container } from 'react-bootstrap';
 import Footer from '../components/Footer';
 import "../css/About.css";
